c65f7eefd2108b3a95240822dd9b0f1683e1381a
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-24 20:32:43 +0200

    update

---
+++ 8-os-module.js
@@ -0,0 +1,18 @@
+const os = require('os')
+const { release } = require('process')
+// info about cureent user
+const user = os.userInfo()
+console.log(user)
+
+// methd returns the system uptime in secs
+
+console.log(`The System Uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+}
+
+console.log(currentOS)
--- app.js
+++ app.js
@@ -1,19 +1,3 @@
-const os = require('os')
-const { release } = require('process')
-// info about cureent user
-const user = os.userInfo()
-console.log(user)
+const path = require('path')
 
-// methd returns the system uptime in secs
-
-console.log(`The System Uptime is ${os.uptime()} seconds`)
-
-const currentOS = {
-    name:os.type(),
-    release: os.release(),
-    totalMem:os.totalmem(),
-    freeMem: os.freemem(),
-}
-
-console.log(currentOS)
-
+console.log(path.sep)
