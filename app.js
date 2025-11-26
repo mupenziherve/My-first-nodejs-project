d086a5d6bcef6454a89ae7bcf6499f1edc00975b
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-26 10:58:46 +0200

    update

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-const { console } = require('inspector')
-const path = require('path')
+const {readFileSync, writeFileSync} = require ('fs')
 
-console.log(path.sep)
-
-const filePath = path.join('./content','subfolder','text.txt')
-console.log(filePath)
-
-const base = path.basename(filePath)
-console.log(base)
-
-const absolute = path.resolve(__dirname, 'content', 'subfolder', 'text.txt')
-console.log(absolute)
