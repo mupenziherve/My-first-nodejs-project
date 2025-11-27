8605e356eb34c3cc732fcba1bd713bcaf87722a4
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-27 23:57:38 +0200

    update

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -7,3 +7,13 @@ writeFileSync(
   './content/result-sync.txt',
   `here is the result : ${first}, ${secord}`, {flag: 'a'}
 )
+
+const { readFileSync, writeFileSync } = require('fs')
+
+const first = readFileSync('./content/first.txt', 'utf8')
+const secord = readFileSync('./content/secord.txt', 'utf8')
+
+writeFileSync(
+  './content/result-sync.txt',
+  `here is the result : ${first}, ${secord}`, {flag: 'a'}
+)
--- 9-path-module.js
+++ 9-path-module.js
@@ -10,3 +10,13 @@ console.log(base)
 
 const absolute = path.resolve(__dirname, 'content', 'subfolder', 'text.txt')
 console.log(absolute)
+const { readFileSync, writeFileSync } = require('fs')
+
+const first = readFileSync('./content/first.txt', 'utf8')
+const secord = readFileSync('./content/secord.txt', 'utf8')
+
+writeFileSync(
+  './content/result-sync.txt',
+  `here is the result : ${first}, ${secord}`,
+  { flag: 'a' }
+)
