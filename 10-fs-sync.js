d670c902e8cebb7d739c0579ee5a6d9720d8e2d8
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-27 23:57:19 +0200

    update

---
+++ 10-fs-sync.js
@@ -0,0 +1,9 @@
+const { readFileSync, writeFileSync } = require('fs')
+
+const first = readFileSync('./content/first.txt', 'utf8')
+const secord = readFileSync('./content/secord.txt', 'utf8')
+
+writeFileSync(
+  './content/result-sync.txt',
+  `here is the result : ${first}, ${secord}`, {flag: 'a'}
+)
