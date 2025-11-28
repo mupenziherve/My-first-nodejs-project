0b6bf68d5f35e60d1410cab5ab4534f66df742c8
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-28 13:33:00 +0200

    update

---
+++ app.js
@@ -0,0 +1,12 @@
+const { readFile, writeFile } = require('fs')
+
+readFile('./content/first.txt', (err, result) => {
+
+    if (err) {
+        console.log(err)
+        return
+        
+    }
+  console.log(result);
+     
+})
