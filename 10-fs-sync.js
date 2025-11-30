839637c5b386738163f73e5032cb60066e1b2e1c
Mupenzi Herve <mupenziherve04@gmail.com>
2025-11-30 22:48:53 +0200

    update

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -8,15 +8,3 @@ writeFileSync(
   `here is the result : ${first}, ${secord}`, {flag: 'a'}
 )
 
-const { readFile, writeFile } = require('fs')
-
-readFile('./content/first.txt', 'utf8', (err, result) => {
-  if (err) {
-    console.log(err)
-    return
-  }
-  const first = result
-  readFile('./content')
-
-  console.log(result)
-})
--- app.js
+++ app.js
@@ -12,15 +12,3 @@ readFile('./content')
   console.log(result);
      
 })
-const { readFile, writeFile } = require('fs')
-
-readFile('./content/first.txt', 'utf8', (err, result) => {
-  if (err) {
-    console.log(err)
-    return
-  }
-  const first = result
-  readFile('./content')
-
-  console.log(result)
-})
