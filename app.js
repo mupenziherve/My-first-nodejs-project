bcd59b2464fdf5c8cb63eafa04e7ff1a6c32c3d2
Mupenzi Herve <mupenziherve04@gmail.com>
2025-12-18 22:32:29 +0200

    update

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
+  if (req.url === '/') {
+    res.end('Welcome To My Home Page')
+    
+  }
+
+  if (req.url === '/about') {
+    res.end('Here is our short history')
+   
+  }
+
+  res.end(`
+    <h1>Oops!</h1>
+    <p> We can't seems to find the page you're loooking for</p>
+    <a href="/">Back Home</a>
+    
+    `);
+  
 
 })
+server.listen(5000)
