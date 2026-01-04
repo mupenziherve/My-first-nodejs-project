4b87eb94f52553f590825996ac4d5e000508337b
Mupenzi Herve <mupenziherve04@gmail.com>
2026-01-04 12:44:13 +0200

    update

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const {createReadStream} = require('fs')
-
-const stream = createReadStream('./content/big.txt')
-
-stream.on('data', (result) => {
-  console.log(result)
-})
-stream.on('error', (err) => console)
+const _ = require('lodash')
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
