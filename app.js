12023f67f034d43f0c7480f2d17f492d3e8006d8
Mupenzi Herve <mupenziherve04@gmail.com>
2026-01-14 11:20:54 +0200

    update

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-const { readFile } = require('fs')
-
-console.log('started a first tast')
-readFile('./content/first.txt', 'utf8', (err, result)=>{
-   if (err) {
-    console.log(err)
-    return
-    
-   } 
-   console.log(result)
-   console.log('completed first task')
-})
-console.log('starting next task')
+// started operating system process
+console.log('first')
+setTimeout(() => {
+  console.log('second')
+}, 0)
+console.log('third')
+// completed and exited operating system process
