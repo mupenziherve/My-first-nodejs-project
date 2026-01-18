4ed53a27240db28c29a2e179b5e7a09e334ab95d
Mupenzi Herve <mupenziherve04@gmail.com>
2026-01-18 11:54:14 +0200

    update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require('http')
 
-const server = http.createServer((req, res) => {
-  console.log('request event')
-  res.end('Hello World')
+const server = http.createServer((req,res) => {
+
+
 })
 
-server.listen(5000, () => {
-  console.log('Server listening on port : 5000....')
+server.listen(5000, () =>{
+    console.log('server Listening on port 5000...')
 })
